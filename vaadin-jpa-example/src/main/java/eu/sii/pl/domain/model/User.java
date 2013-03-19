06d7919eefcfd81c275a0d4de3a728b64e72957f
Wojciech Kochman <wojciech_kochman@yahoo.com>
2013-03-19 15:22:33 +0100

    jpa

---
+++ vaadin-jpa-example/src/main/java/eu/sii/pl/domain/model/User.java
@@ -0,0 +1,46 @@
+package eu.sii.pl.domain.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String lastName;
+    @Column
+    private Date birthday;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Date getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+    
+}
