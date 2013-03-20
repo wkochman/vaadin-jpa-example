52b3bf1a81cbce3cc86c793a4a63abecb09b4501
Wojciech Kochman <wojciech_kochman@yahoo.com>
2013-03-20 08:13:29 +0100

    table

--- vaadin-jpa-example/src/main/java/eu/sii/pl/domain/utils/DBUtils.java
+++ vaadin-jpa-example/src/main/java/eu/sii/pl/domain/utils/DBUtils.java
@@ -1,10 +1,44 @@
 package eu.sii.pl.domain.utils;
 
+import java.util.Date;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import com.vaadin.addon.jpacontainer.JPAContainer;
+import com.vaadin.addon.jpacontainer.provider.CachingMutableLocalEntityProvider;
+
+import eu.sii.pl.domain.model.User;
 
 public class DBUtils {
     public static EntityManager getEntityManager() {
         return Persistence.createEntityManagerFactory("test").createEntityManager();
+    }
+
+    public static JPAContainer<User> getUsersContainer() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+
+        EntityManager em = emf.createEntityManager();
+
+        CachingMutableLocalEntityProvider<User> entityProvider = new CachingMutableLocalEntityProvider<User>(User.class, em);
+
+        JPAContainer<User> persons = new JPAContainer<User>(User.class);
+        persons.setEntityProvider(entityProvider);
+        return persons;
+    }
+
+    public static void populate() {
+        EntityManager entityManager = getEntityManager();
+        entityManager.getTransaction().begin();
+        for (int i = 0; i < 10000; i++) {
+            User u = new User();
+            u.setId(Long.valueOf(i));
+            u.setName("f_name_" + i);
+            u.setLastName("l_name_" + i);
+            u.setBirthday(new Date());
+            entityManager.persist(u);
+        }
+        entityManager.getTransaction().commit();
     }
 }
--- vaadin-jpa-example/src/main/java/eu/sii/pl/view/MyVaadinUI.java
+++ vaadin-jpa-example/src/main/java/eu/sii/pl/view/MyVaadinUI.java
@@ -1,11 +1,16 @@
 package eu.sii.pl.view;
 
+import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+
+import eu.sii.pl.domain.model.User;
+import eu.sii.pl.domain.utils.DBUtils;
 
 /**
  * The Application's "main" class
@@ -19,14 +24,19 @@ public class MyVaadinUI extends UI
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
-        
-        Button button = new Button("Click Me");
+        final JPAContainer<User> container = DBUtils.getUsersContainer();
+        Table table = new Table(null, container);
+        table.setSelectable(true);
+        Button button = new Button("Populate");
         button.addClickListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
+                DBUtils.populate();
+                container.refresh();
+                Notification.show("Data populated", Notification.Type.TRAY_NOTIFICATION);
             }
         });
         layout.addComponent(button);
+        layout.addComponent(table);
     }
 
 }
